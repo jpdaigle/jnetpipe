d927da3c0ab53cdcb2a63913e2a3320856533a06
jpdaigle <jpdaigle@gmail.com>
2009-08-06 15:41:41 -0400

    patched run script to have correct path separators in cygwin/unix

--- bin_staged/jnetpipe.sh
+++ bin_staged/jnetpipe.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd `dirname $0`
-java -cp "./bsh-2.0b4.jar;./jnetpipe.jar;./log4j-1.2.9.jar" com.solacesystems.testtool.jnetpipe.JNetPipe $@
+if [[ $OSTYPE == 'cygwin' ]]; then sep=';'
+else sep=':'
+fi
+
+# Build a classpath with our jarfiles, using the platform-specific path sep
+jarcp="./bsh-2.0b4.jar$sep./jnetpipe.jar$sep./log4j-1.2.9.jar"
+java -cp "$jarcp" com.solacesystems.testtool.jnetpipe.JNetPipe $@
 
